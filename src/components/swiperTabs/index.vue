3692662fbff762c1a2a381d3a393ecc5bb5e6e51
xlf <my_hero_IronMan@outlook.com>
2020-05-07 20:25:24 +0800

    Update index.vue

--- src/components/swiperTabs/index.vue
+++ src/components/swiperTabs/index.vue
@@ -1,5 +1,5 @@
 <script lang="tsx">
-import { Component, Vue, Prop, Watch } from 'vue-property-decorator';
+import { Component, Vue, Prop, Watch, Provide } from 'vue-property-decorator';
 import { CreateElement } from 'vue';
 
 interface ComponentData {
@@ -33,6 +33,42 @@ interface ComponentData {
     },
 })
 export default class swiperTabs extends Vue {
+
+    @Prop({type:String,default:''}) value: String;
+
+
+    @Provide() rootTabs = this;
+
+    private currentName: String = this.value;
+    private panes: Array<any> = [];
+
+    @Watch('value')
+    valueWatch(val:String){
+        this.setCurrentName(val);
+    }
+
+    //methods
+    private setCurrentName(val: String):void{};
+
+    // get the pane instance
+    private calcPaneInstances(isForceUpdate:Boolean = false):void{
+        if(this.$slots.default){
+            const paneSlots = this.$slots.default.filter(vnode=>
+                            vnode.tag && 
+                            vnode.componentOptions &&
+                            (vnode.componentOptions.Ctor as any).options.name === 'TabPane' );
+                            
+            const panes = paneSlots.map(({componentInstance})=>componentInstance);
+            const panesChanged = !(panes.length === this.panes.length && panes.every((pane,index)=>pane === this.panes[index]));
+            if(isForceUpdate || panesChanged){
+                this.panes = panes;
+            }
+
+        }else if(this.panes.length !== 0){
+            this.panes = [];
+        }
+    }
+
 
 }
 
